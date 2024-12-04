002e7ac8cc5835625d4d9669728d5cd707218147
Rawan-Rawan <rawansalahat1@gmail.com>
2024-12-04 18:10:12 +0200

    assign 24

---
+++ Assets/Script/Assign 24/RaceState.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+namespace Assign24
+{
+    public enum RaceState
+    {
+        Start,
+        Accelerate,
+        Turn,
+        Crash,
+        Finish,
+    }
+}
